349dda0a01802cf01440b713076c8150fe1318ca
Vince216 <vinceshury@gmail.com>
2019-07-05 02:55:33 -0400

    changed code for root route for sending react files in dev

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,13 @@ router.use("/api", apiRoutes);
 
 // =========== SEND REACT PRODUCTION BUILD ====================
 router.get("*", (req, res) => {
-    console.log("Attempting to Send production build");
     if (process.env.NODE_ENV === "production") {
         res.sendFile(path.join(__dirname, "../client/build/index.html"));
     }
-    res.status(400);
+    else{ 
+        console.log("Attempted to send Production build.")
+        res.status(400);
+    }
 });
 
 module.exports = router;
