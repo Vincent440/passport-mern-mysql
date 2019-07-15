2215a1935edfe6be9e9bef31c83c7d7bce9fafe5
Vince216 <vinceshury@gmail.com>
2019-07-15 16:47:22 -0400

    removed jawsdb_url conditional, moved db creds to env file and heroku settings

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
 const mysql = require("mysql2");
 
-const options = {
+
+const connection = mysql.createConnection({
     host: process.env.DB_HOST, // Host name for database connection:
     port: process.env.DB_PORT,// Port number for database connection:
     user: process.env.DB_USER,// Database user:
     password: process.env.DB_PW,// Password for the above database user:
     database: process.env.DB_NAME// Database name:
-}
-const connection = process.env.JAWSDB_URL ?
-(mysql.createConnection(process.env.JAWSDB_URL)) : (mysql.createConnection(options));
+});
 connection.connect();
 
 module.exports = connection;
