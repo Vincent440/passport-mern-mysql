61d854ec72256cfe467cf8a69ac771a1faee1a6b
Vince216 <vinceshury@gmail.com>
2019-07-15 16:23:23 -0400

    testing out using the jawsdb url for deployment instead of env variables

--- config/promiseConnection.js
+++ config/promiseConnection.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
-const sessionOptions = {
+const sessionOptions = process.env.JAWSDB_URL ? (process.env.JAWSDB_URL) : ({
     host: process.env.DB_HOST, // Host name for database connection:
     port: process.env.DB_PORT,// Port number for database connection:
     user: process.env.DB_USER,// Database user:
@@ -11,7 +11,7 @@ const sessionOptions = {
     clearExpired: true,// Whether or not to automatically check for and clear expired sessions:
     checkExpirationInterval: 900000,// How frequently expired sessions will be cleared; milliseconds:
     expiration: 7200000// The maximum age of a valid session; 2 hours in milliseconds:
-}
+});
 const sessionStore = new MySQLStore(sessionOptions, connection.promise);
 
 module.exports = sessionStore;
