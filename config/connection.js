c090226f7f093b7ba69e32be028afc91213ea420
Vince216 <vinceshury@gmail.com>
2019-07-15 16:39:35 -0400

    renamed db env variable

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const options = {
     host: process.env.DB_HOST, // Host name for database connection:
     port: process.env.DB_PORT,// Port number for database connection:
     user: process.env.DB_USER,// Database user:
-    password: process.env.DB_PASSWORD,// Password for the above database user:
+    password: process.env.DB_PW,// Password for the above database user:
     database: process.env.DB_NAME// Database name:
 }
 const connection = process.env.JAWSDB_URL ?
--- config/promiseConnection.js
+++ config/promiseConnection.js
@@ -6,7 +6,7 @@ const sessionOptions = {
     host: process.env.DB_HOST, // Host name for database connection:
     port: process.env.DB_PORT,// Port number for database connection:
     user: process.env.DB_USER,// Database user:
-    password: process.env.DB_PASSWORD,// Password for the above database user:
+    password: process.env.DB_PW,// Password for the above database user:
     database: process.env.DB_NAME,// Database name:,
     clearExpired: true,// Whether or not to automatically check for and clear expired sessions:
     checkExpirationInterval: 900000,// How frequently expired sessions will be cleared; milliseconds:
