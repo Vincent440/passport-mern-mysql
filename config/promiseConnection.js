8e1d86d811fc8c195aea1949b89d60417c0e3a3d
Vince216 <vinceshury@gmail.com>
2019-07-15 16:16:04 -0400

    working sessions testing deployed

--- config/promiseConnection.js
+++ config/promiseConnection.js
@@ -1,16 +1,6 @@
-const mysql = require("mysql2/promise");
+const connection = require('./connection');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
-
-const options = {
-    host: process.env.DB_HOST, // Host name for database connection:
-    port: process.env.DB_PORT,// Port number for database connection:
-    user: process.env.DB_USER,// Database user:
-    password: process.env.DB_PASSWORD,// Password for the above database user:
-    database: process.env.DB_NAME// Database name:
-}
-const connection = process.env.JAWSDB_URL ?
-(mysql.createConnection(process.env.JAWSDB_URL)) : (mysql.createConnection(options));
 
 const sessionOptions = {
     host: process.env.DB_HOST, // Host name for database connection:
