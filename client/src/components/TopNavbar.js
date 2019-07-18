38d670829deb108f1adaf7ce2f832e2006e2a862
Vince216 <vinceshury@gmail.com>
2019-07-18 11:31:14 -0400

    updated top navbar with NavLink instead of Link from React router lib, allowing for active styling based on current route path

--- client/src/components/TopNavbar.js
+++ client/src/components/TopNavbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import LoginLogoutLink from './LoginLogoutLink';
 import { withRouter } from "react-router";
 
@@ -11,10 +11,10 @@ class TopNavbar extends React.Component {
       <Navbar className="mx-0" bg="dark" variant="dark">
         <Navbar.Brand className='text-capitalize'>Mern Passport MySQL App Welcome: {this.props.user.username}</Navbar.Brand>
         <Nav className="px-2">
-          <Link to="/" className="nav-link">Dashboard</Link>
-          <Link to="/manager" className="nav-link">Manager Dashboard</Link>
-          <Link to="/admin" className="nav-link">Admin Dashboard</Link>
-          <Link to="/about" className="nav-link">About</Link>
+          <NavLink exact to="/" className="nav-link" activeClassName="active" >Dashboard</NavLink>
+          <NavLink exact to="/manager" className="nav-link" activeClassName="active" >Manager Dashboard</NavLink>
+          <NavLink exact to="/admin" className="nav-link" activeClassName="active" >Admin Dashboard</NavLink>
+          <NavLink exact to="/about" className="nav-link" activeClassName="active" >About</NavLink>
           <LoginLogoutLink loggedIn={this.props.loggedIn} />
         </Nav>
         <h6 className="text-white text-capitalize">{this.props.loggedIn ? `Username: ${this.props.user.username} Access-Lvl: ${this.props.user.type}` : 'Please Log In.'}</h6>
