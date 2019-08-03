268170086946b7a41461fd53f515600f45e5d354
vince s <vincepsh@live.com>
2019-08-03 11:36:38 -0400

    updated seeds file pw location to be easier to drop into workbench

--- config/seeds.sql
+++ config/seeds.sql
@@ -7,10 +7,15 @@ set foreign_key_checks=0;
 TRUNCATE TABLE users;
 
 INSERT INTO users (username, password, access_id) VALUES
-('admin', '$2b$10$Qf/0VNBQiYr.pHN8IN9Yl.SrYQCpG4b2mrsX6dx85DkE7/fwsNWvy', 3), -- pw admin
-('manager', '$2b$10$2ataQ4kjDbZaR9TDSUqXI.Mt.Gq/bDn1Te3MN939s3fJAtTrKJa9i', 2),--pw 123456
-('vince', '$2b$10$HEj.issBvH6pxDEiBxtCr.c8fU7Cl4TC34K4/MGtTMdXqztLyqt8K', 3),--pw 654321
-('user', '$2b$10$rnUz2cFk61G27KdixeR5G.rf78zaKzDZlebrs9ZK5tnnVGFWrnUPm', 1);-- pw 123123
+
+('admin', '$2b$10$Qf/0VNBQiYr.pHN8IN9Yl.SrYQCpG4b2mrsX6dx85DkE7/fwsNWvy', 3),
+-- pw admin
+('manager', '$2b$10$2ataQ4kjDbZaR9TDSUqXI.Mt.Gq/bDn1Te3MN939s3fJAtTrKJa9i', 2),
+-- pw 123456
+('vince', '$2b$10$HEj.issBvH6pxDEiBxtCr.c8fU7Cl4TC34K4/MGtTMdXqztLyqt8K', 3),
+-- pw 654321
+('user', '$2b$10$rnUz2cFk61G27KdixeR5G.rf78zaKzDZlebrs9ZK5tnnVGFWrnUPm', 1);
+-- pw 123123
 
 -- --------------------------------------------------------
 --
