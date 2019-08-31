355a3b3114acf3587823e7a6152e34a4af741fbb
vince s <vinceshury@gmail.com>
2019-08-31 07:24:21 -0400

    updated npm packages

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 class About extends React.Component {
   render() {
-    return(
+    return (
       <div className="p-4 border">
         <h1 className="display-1 text-center text-white">About Page!</h1>
         <h1 className="text-center">Welcome to the about page.</h1>
         <p className="text-center">You should have to be logged in, in order to view the contents of this page without being immediately redirected.</p>
       </div>
-
     );
   }
 }
