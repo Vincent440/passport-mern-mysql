7fa492ef647d45dcf2457bbb83da76d342496cb3
Vincent Shury <vinceshury@gmail.com>
2023-04-13 15:20:24 -0400

    Added test example change

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+// import Example from './Example'
+// ReactDOM.render(<Example />, document.getElementById('root'))
 
-import Example from './Example'
-ReactDOM.render(<Example />, document.getElementById('root'))
-
-// import App from './App'
-// ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App'
+ReactDOM.render(<App />, document.getElementById('root'))
